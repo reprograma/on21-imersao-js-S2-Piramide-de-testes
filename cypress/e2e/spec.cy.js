da3b450441a776ffe3ece121d5ba3f95edb9bcb6
Rosalyne Duarte <rosalyneduarte@gmail.com>
2022-09-24 12:43:28 -0300

    Salvar Informacoes

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,13 @@
+//const { expect } = require("chai")
+
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+describe('meu primeiro teste', () => {
+  it('Passa por favor :D', () => {
+    expect(true).to.equal(true)
+  })
+})
