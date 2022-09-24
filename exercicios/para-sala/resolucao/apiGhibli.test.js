3f23360057d2da457ffb3b048722c0f82475b59d
Leticia Luzia <leticia.luzia@thoughtworks.com>
2022-09-24 01:52:56 -0300

    Exercício para sala

---
+++ exercicios/para-sala/resolucao/apiGhibli.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const ApiUrl = "https://ghibliapi.herokuapp.com";
+
+describe("GET /films/{id}", () => {
+    test("deve retornar o filme meu amigo totoro", () => {
+        return request(ApiUrl)
+            .get("/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49")
+            .expect(200)
+            .then(response => {
+                expect(response.body.title).toEqual("My Neighbor Totoro")
+            })
+    })
+})
