e9561434c3e2fb1e12798ad5184740d53b40fc6f
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-25 14:10:58 -0300

    :computer: test: add apiGhibli

--- exercicios/para-sala/resolucao/apiGhibli.test.js
+++ exercicios/para-sala/resolucao/apiGhibli.test.js
@@ -1,13 +1,48 @@
-const request = require('supertest');
+const request = require("supertest");
 const ApiUrl = "https://ghibliapi.herokuapp.com";
 
 describe("GET /films/{id}", () => {
-    test("deve retornar o filme meu amigo totoro", () => {
-        return request(ApiUrl)
-            .get("/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49")
-            .expect(200)
-            .then(response => {
-                expect(response.body.title).toEqual("My Neighbor Totoro")
-            })
+  beforeEach(() => {
+    request(ApiUrl);
+  });
+  test("deve retornar o filme meu amigo totoro", () => {
+    return request(ApiUrl)
+      .get("/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.title).toEqual("My Neighbor Totoro");
+      });
+  });
+
+  test("deve retornar o filme meu amigo totoro", () => {
+    const returno = request(ApiUrl)
+      .get("/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.title).toEqual("My Neighbor Totoro");
+      });
+  });
+
+  test("deve retornar 404 ao passar um id inválido", () => {
+    request(ApiUrl)
+      .get("/films/58611129-2dbc-4a81-a72f-77ddfc1b1b4")
+      .expect(404)
+      .then((response) => {
+        expect(response.statusCode).toEqual(404);
+      });
+  });
+  
+});
+
+
+describe('GET /people', () => {
+    test('Deve retornar 200 e encontrar o nome Chihiro Ogino', () => {
+        request(ApiUrl)
+        .get('/people')
+        .expect(200)
+        .then(response => {
+            expect(response.body).toEqual(expect.arrayContaining
+                ([expect.objectContaing({'name': 'Chihiro Ogino'})]))
+        })
     })
 })
