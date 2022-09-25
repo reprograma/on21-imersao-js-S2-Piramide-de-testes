25fcff213b2213d6fdea58897273aa9d5672c1cf
linefmv <alinemaciel018@gmail.com>
2022-09-25 13:48:41 -0300

    build: cypress config

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io') // cypress visita essa página
+  })
+})
+
+describe("Meu primero teste", () => {
+  it("passa por favor", () => {
+    expect(true).to.equal(true)
+  })
+});
