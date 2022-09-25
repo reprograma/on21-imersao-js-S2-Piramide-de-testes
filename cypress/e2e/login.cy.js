8b0c546c79dc64c6021b0338312b0e78f3c24043
amrodrigues <42442977+amrodrigues@users.noreply.github.com>
2022-09-25 17:26:28 -0300

    Atualizando

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,9 @@
+describe('Login', () => {
+    it('Deve realizar login com sucesso', () => {
+      cy.visit('http://automationpractice.com/index.php')
+      cy.get('.login').click()
+      cy.get('#email').type('teste2021@teste.com.br')
+      cy.get('#passwd').type('teste')
+      cy.get('#SubmitLogin').click()
+    })
+  })
--- exercicios/para-sala/resolucao/apiGhibli.test.js
+++ exercicios/para-sala/resolucao/apiGhibli.test.js
@@ -10,4 +10,47 @@ describe("GET /films/{id}", () => {
                 expect(response.body.title).toEqual("My Neighbor Totoro")
             })
     })
+
+    test("deve retornar 200 o filme Grave of the Fireflies", () => {
+        return request(ApiUrl)
+            .get("/films/12cfb892-aac0-4c5b-94af-521852e46d6a")
+            .expect(200)
+            .then(response => {
+                expect(response.body.title).toEqual("Grave of the Fireflies")
+            })
+    })
+
+    test("deve retornar 404  o filme Grave of the Fireflies", () => {
+        request(ApiUrl)
+            .get("/films/000")
+            .expect(404)
+            .then(response => {
+                expect(response.statusCode).toEqual(404)
+            })
+    })
+})
+
+describe("Get /people" ,()=> {
+
+    test("deve retornar 200 e encontrar a personagem Chihiro", ()=> {
+        const variavel = {"name":"Chihiro Ogino"}
+        const resultado = (expect.arrayContaining([expect.objectContaining(variavel)]))
+        request (ApiUrl)
+            .get("/people")
+            .expect(200)
+            .then (response =>{
+                expect((response.body)).toEqual(resultado)
+            })
+    })
+
+    test("deve retornar 200 e encontrar a personagem Ashitaka", ()=> {
+        const variavel = {"name": "Ashitaka"}
+        const resultado = (expect.arrayContaining([expect.objectContaining(variavel)]))
+        request (ApiUrl)
+            .get("/people")
+            .expect(200)
+            .then (response =>{
+                expect((response.body)).toEqual(resultado)
+            })
+    })
 })
