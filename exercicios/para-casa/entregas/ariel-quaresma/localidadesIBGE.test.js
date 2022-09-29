d1c436e048dfc7ba23984885bc2c98b212763de8
Ariel Quaresma <arielquaresma90@gmail.com>
2022-09-29 11:13:58 -0300

    Projeto exercicio 1 finalizado, com 6 testes feitos baseados na UF PI da aAPI IBGELocalidades

---
+++ exercicios/para-casa/entregas/ariel-quaresma/localidadesIBGE.test.js
@@ -0,0 +1,61 @@
+const request = require('supertest');
+const ApiUrl = "https://servicodados.ibge.gov.br"
+
+
+describe("GET/estados/PI", () =>{
+    test("it check if we can get UF of PI by id and should return id 22", () => {
+
+        request(ApiUrl)
+        .get('/api/v1/localidades/estados/22')
+        .expect(200)
+        .then(response => {
+            expect(response.body.id).toEqual(22)
+        })
+    })
+
+    test("it checks if get a empty array with an invalid id", () => {
+        request(ApiUrl)
+        .get('/api/v1/localidades/estados/700')
+        .expect(200)
+        .then(response => {
+            expect(response.body).toEqual([])
+        })
+    })
+
+    test("it checks if there's the NE region in the result of PI UF", () => {
+        request(ApiUrl)
+        .get('/api/v1/localidades/estados/22')
+        .expect(200)
+        .then(response => {
+            expect(response.body.regiao.sigla).toEqual("NE")
+        })
+    })
+
+    test("it checks if there's the NE region in the result of PI UF", () => {
+        
+        request(ApiUrl)
+        .get('/api/v1/localidades/estados')
+        .expect(200)
+        .then(response => {
+            expect(response.body).toEqual(expect.arrayContaining([expect.objectContaining({"nome": "Piauí"})]))
+        })
+    })
+
+    test("it checks if there's a city containing in UF array", () => {
+        request(ApiUrl)
+        .get('/api/v1/localidades/estados/22/municipios')
+        .expect(200)
+        .then(response => {
+            expect(response.body).toEqual(expect.arrayContaining([expect.objectContaining({"nome":"Teresina"})]))
+        })
+    })
+
+    test("it checks if a id city not containing in UR array will back a empty array", () => {
+        request(ApiUrl)
+        .get('/api/v1/localidades/estados/22/municipios')
+        .expect(200)
+        .then(response => {
+            expect(response.body).toEqual(expect.not.arrayContaining([{"id":4}]))
+        })
+    })
+})
--- exercicios/para-sala/resolucao/apiGhibli.test.js
+++ exercicios/para-sala/resolucao/apiGhibli.test.js
@@ -1,8 +1,16 @@
+//Importar para o arquivo o supertest e a url da API que será utilizada
+//Lembrar de puxar o caminho feliz e o caminho triste também
+
 const request = require('supertest');
 const ApiUrl = "https://ghibliapi.herokuapp.com"
 
+//Na descrição de utilização de APIs geralmente colocamos o método e a rota que iremos
+//puxar para testar
 describe("GET /films/{id}", () => {
     test("deve retornar 200 e encontrar o filme Castle in the Sky na lista", () => {
+        //Primeiro, chamar a const que recebeu a url da API, depois o método que será testado
+        //com o seu parâmetro (no caso, com o ID), depois um expect com o 200 (dentro das 200 opções que a api fornece)
+        //e no then seria a resposta, com a estrutura expect do jest/testes unitários
         request(ApiUrl)
             .get("/films/2baf70d1-42bb-4437-b551-e5fed5a87abe")
             .expect(200)
@@ -22,6 +30,7 @@ describe("GET /films/{id}", () => {
 
 describe("GET /people", () => {
     test("deve retornar 200 e encontrar a personagem Chihiro", () => {
+        
         request(ApiUrl)
             .get("/people")
             .expect(200)
