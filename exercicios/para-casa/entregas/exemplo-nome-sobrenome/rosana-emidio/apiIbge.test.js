9c03bb30f2a9d8706df0269bae72f7d8a8e8dc1f
rosanaemidio <rosanaemidio42@gmail.com>
2022-09-30 01:40:00 -0300

    adicionado testesna apoi do ibge

--- exercicios/para-casa/entregas/exemplo-nome-sobrenome/rosana-emidio/apiIbge.test.js
+++ exercicios/para-casa/entregas/exemplo-nome-sobrenome/rosana-emidio/apiIbge.test.js
@@ -1,29 +1,49 @@
+
 const request = require('supertest')
 const ApiUrl = "https://servicodados.ibge.gov.br/api/docs/localidades"
 
-describe('GET /regioes', () => {
+describe('Informações do IBGE', () => {
     
-    test('deve retornar 200 quando buscar pelo municipio de Ananindeua', () => {
-        const estadoPara = () => expect.arrayContaining([expect.objectContaining({ "nome": "Pará" })])
-        const municipioAnanindeua = estadoPara([expect.objectContaining({ "nome":"Ananindeua" })])
+    test('deve retornar 200 quando buscar pelo municipio de Beberibe', () => {
+        const estadoCeara = () => expect.arrayContaining([expect.objectContaining({ "nome": "Ceará" })])
+        const municipioBeberibe = estadoCeara([expect.objectContaining({ "nome":"Beberibe" })])
 
         request(ApiUrl)
             .get('/estados')
             .expect(200)
             .then(response => {
-                expect(response.body).toEqual(municipioAnanindeua)
+                expect(response.body).toEqual(municipioBeberibe)
             })
     })
 
-    test("deve retornar 200 id:290010805", () => {
+    test('deve retornar 200 ao pesquisar o id 355030801', () => {
         request (ApiUrl)
-            .get("/distritos/BA")
+            .get("/distritos/SP")
             .expect(200)
             .then (response =>{
-                expect((response.body[0].id)).toEqual(290010805)
-
+                expect((response.body[0].id)).toEqual(355030801)
             })
     })
-})
 
+    test('deve retornar 200 quando buscar distrito do Jardim Ângela', () => {
+        const jardimAngela = () => expect.arrayContaining([expect.objectContaining({ "nome": "Jardim Ângela" })])
+
+        request(ApiUrl)
+            .get('/municipios/3550308/distritos')
+            .expect(200)
+            .then(response => {
+                expect(response.body).toEqual(jardimAngela)
+            })
+    })
+
+    test('Deve retornar a região nordeste', () => {
+     request(ApiUrl)
+     .get('/regioes?orderBy=nome')
+     .expect(200)
+     .then(response =>{
+        expect(response.body[1].nome).toEqual('Nordeste')
+     })
+    })
+
+})
 
