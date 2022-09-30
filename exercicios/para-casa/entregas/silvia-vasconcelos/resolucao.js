fc7f907f91eeb1384be2ff75ee76b3ed36699b69
Silvovisk <vascsilvia@gmail.com>
2022-09-29 23:29:53 -0300

    atualizando piramides de testes

---
+++ exercicios/para-casa/entregas/silvia-vasconcelos/resolucao.js
@@ -0,0 +1,9 @@
+// Cenário: Configurar tarefas
+    // Dado que eu acesso o site
+    // E realizei previamente a criação do meu login
+    // E me logar
+    // Quando clicar no botão "Adicionar tarefa"
+    // E escolher a opção "Afazer"
+    // E digitar obrigatoriamente um Título e editar as informações necessárias
+    // E clicar no botão "Criar"
+    // Então vejo que foi criado a minha Tarefa na coluna de Afazeres. 
